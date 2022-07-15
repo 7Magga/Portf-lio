c95ee34418e5d3a8990451e451621d0743dac56d
Magga <matheus.dev.gabryel@outlook.com>
2022-07-15 16:07:54 -0300

    Alterações Layout

---
+++ GPDHL/GPDHL/Controllers/HomeController1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GPDHL.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        public JsonResult Index()
+        {
+            var a = "Test";
+            return Json(a);
+        }
+    }
+}
